5ae3d135855107bc981953d8866515cc6944ecf5
RatnakarPatil <ratnakarpatil2003@gmail.com>
2024-04-21 20:56:17 +0530

    Assg7-python-flask-app

--- Assg7/Flask-app/app.py
+++ Assg7/Flask-app/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0',port=int("3000"),debug=True)
